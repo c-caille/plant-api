fb0c83f981fa9b7f2d97c12de131d02adcb7c4ca
Christian Caille <christian.caille@gmail.com>
2022-07-18 21:30:50 -0700

    initial plant api project

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 8000
+
+app.use(cors())
+
+const plants = {
+    'garry oak': {
+        'speciesName' : 'Garry Oak',
+        'habitat': 'Rocky',
+        'height' : 2
+    }, 
+      
+    'invasive': {
+        'speciesName' : 'Invasive',
+        'habitat': 'disturbed',
+        'height' : 1
+      
+    },
+
+    'camus': {
+        'speciesName': 'Common Camus',
+        'habitat': 'Meadow',
+        'height': 2,
+    }
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:plantName', (request, response) => {
+    const speciesName = request.params.plantName.toLowerCase()
+    if(plants[speciesName]){
+        response.json(plants[speciesName])
+    } else {
+        response.json(plants ['invasive'])
+    }
+})
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log('Server is running.')
+})
